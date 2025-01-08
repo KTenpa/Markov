ba3e1e05496accd520937f98f024c3ee57f8cfd5
Karma Tenpa <ktenpa@mac.mynetworksettings.com>
2025-01-07 19:34:08 -0600

    Implement Markov Chain text generator
    - Added MarkovMachine class to generate random text based on input.
    - Constructor splits input text into words and constructs a Markov Chain.
    - makeChains method builds the transition map between words.
    - makeText method generates random text by following word transitions.
    - Static choice method added to randomly select an element from an array.

--- markov.js
+++ markov.js
@@ -1,9 +1,4 @@
-/** Textual markov chain generator */
-
-
 class MarkovMachine {
-
-  /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
@@ -11,19 +6,32 @@ class MarkovMachine {
     this.makeChains();
   }
 
-  /** set markov chains:
-   *
-   *  for text of "the cat in the hat", chains will be
-   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
-
   makeChains() {
-    // TODO
+    const chains = new Map();
+  
+    this.words.forEach((word, i) => {
+      const nextWord = this.words[i + 1] || null;
+      chains.has(word)
+        ? chains.get(word).push(nextWord)
+        : chains.set(word, [nextWord]);
+    });
+  
+    this.chains = chains;
+  }
+  
+  static choice(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
   }
 
-
-  /** return random text from chains */
-
   makeText(numWords = 100) {
-    // TODO
+    let keys = Array.from(this.chains.keys());
+    let key = MarkovMachine.choice(keys);
+    let out = [];
+
+    while (out.length < numWords && key !== null) {
+      out.push(key);
+      key = MarkovMachine.choice(this.chains.get(key));
+    }
+    return out.join(" ");
   }
 }
