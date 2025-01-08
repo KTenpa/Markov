211b3493ef21f9b94b7478e69957f09965181fb2
Karma Tenpa <ktenpa@mac.mynetworksettings.com>
2025-01-07 20:08:18 -0600

    Add documentation for MarkovMachine class and methods

--- markov.js
+++ markov.js
@@ -1,11 +1,22 @@
+/**
+ * MarkovMachine generates random text based on input text using a Markov chain model.
+ */
 class MarkovMachine {
 
+    /**
+   * Creates an instance of the MarkovMachine.
+   * @param {string} text - The input text from which to build the Markov chains.
+   */
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
     this.words = words.filter(c => c !== "");
     this.makeChains();
   }
 
+    /**
+   * Creates a map of Markov chains where the keys are words and the values are arrays
+   * of the words that can follow the key word in the given input text.
+   */
   makeChains() {
     const chains = new Map();
   
@@ -23,6 +34,11 @@ class MarkovMachine {
     return arr[Math.floor(Math.random() * arr.length)]
   }
 
+    /**
+   * Generates random text based on the Markov chains.
+   * @param {number} [numWords=100] - The number of words to generate in the output text.
+   * @returns {string} The generated text.
+   */
   makeText(numWords = 100) {
     let keys = Array.from(this.chains.keys());
     let key = MarkovMachine.choice(keys);
